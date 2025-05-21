cb43e12a1ca0a63f9b7fcafc19a7f0d4f98f7f2e
firekidnetzy <firekidchisom@gmail.com>
2025-05-21 16:22:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("addAdButton").addEventListener("click", function() {
+    alert("Feature Coming Soon: Allow businesses to submit ads!");
+});
+document.getElementById("addAdButton").addEventListener("click", () => {
+    document.getElementById("adForm").classList.remove("hidden");
+});
